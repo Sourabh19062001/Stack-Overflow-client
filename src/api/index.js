3ce23430718766bb2a96237ef4e596cc299f0bf3
Sourabh19062001 <sourabhhembrom1@gmail.com>
2024-05-20 11:50:05 +0530

    deploy

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://stack-overflow-server-seven.vercel.app/",
 });
 
 API.interceptors.request.use((req) => {
